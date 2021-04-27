377e9c1221aa0b4abe57dbbc63f726ab26814bc4
QinYunshu <3200115551@zju.edu.cn>
2021-04-28 00:29:12 +0800

    P11

---
+++ Practical11/Alignment.py
@@ -0,0 +1,82 @@
+import os
+import re
+global amino
+global blosum
+os.chdir("C:/Users/Lenovo/IBI1_2020-21/Practical11")
+sequence_human=open('SOD2_human.fa','r')
+sequence_mouse=open('SOD2_mouse.fa','r')
+sequence_random=open('RandomSeq.fa','r')
+file1=sequence_human.read()
+file2=sequence_mouse.read()
+file3=sequence_random.read()
+#to get the sequences we need
+human_=re.findall(r'\w+',file1)
+human=human_[3]
+mouse_=re.findall(r'\w+',file2)
+mouse=mouse_[3]
+random_=re.findall(r'\w+',file3)
+random=random_[1]
+#
+amino = ['A', 'R', 'N', 'D', 'C', 'Q', 'E', 'G', 'H', 'I', 'L', 'K', 'M', 'F', 'P', 'S', 'T', 'W', 'Y', 'V', 'B', 'Z', 'X', '*']
+blosum = [
+[ 4, -1, -2, -2,  0, -1, -1,  0, -2, -1, -1, -1, -1, -2, -1,  1,  0, -3, -2,  0, -2, -1,  0, -4],
+[-1,  5,  0, -2, -3,  1,  0, -2,  0, -3, -2,  2, -1, -3, -2, -1, -1, -3, -2, -3, -1,  0, -1, -4],
+[-2,  0,  6,  1, -3,  0,  0,  0,  1, -3, -3,  0, -2, -3, -2,  1,  0, -4, -2, -3,  3,  0, -1, -4],
+[-2, -2,  1,  6, -3,  0,  2, -1, -1, -3, -4, -1, -3, -3, -1,  0, -1, -4, -3, -3,  4,  1, -1, -4],
+[ 0, -3, -3, -3,  9, -3, -4, -3, -3, -1, -1, -3, -1, -2, -3, -1, -1, -2, -2, -1, -3, -3, -2, -4],
+[-1,  1,  0,  0, -3,  5,  2, -2,  0, -3, -2,  1,  0, -3, -1,  0, -1, -2, -1, -2,  0,  3, -1, -4],
+[-1,  0,  0,  2, -4,  2,  5, -2,  0, -3, -3,  1, -2, -3, -1,  0, -1, -3, -2, -2,  1,  4, -1, -4],
+[ 0, -2,  0, -1, -3, -2, -2,  6, -2, -4, -4, -2, -3, -3, -2,  0, -2, -2, -3, -3, -1, -2, -1, -4],
+[-2,  0,  1, -1, -3,  0,  0, -2,  8, -3, -3, -1, -2, -1, -2, -1, -2, -2,  2, -3,  0,  0, -1, -4],
+[-1, -3, -3, -3, -1, -3, -3, -4, -3,  4,  2, -3,  1,  0, -3, -2, -1, -3, -1,  3, -3, -3, -1, -4],
+[-1, -2, -3, -4, -1, -2, -3, -4, -3,  2,  4, -2,  2,  0, -3, -2, -1, -2, -1,  1, -4, -3, -1, -4],
+[-1,  2,  0, -1, -3,  1,  1, -2, -1, -3, -2,  5, -1, -3, -1,  0, -1, -3, -2, -2,  0,  1, -1, -4],
+[-1, -1, -2, -3, -1,  0, -2, -3, -2,  1,  2, -1,  5,  0, -2, -1, -1, -1, -1,  1, -3, -1, -1, -4],
+[-2, -3, -3, -3, -2, -3, -3, -3, -1,  0,  0, -3,  0,  6, -4, -2, -2,  1,  3, -1, -3, -3, -1, -4],
+[-1, -2, -2, -1, -3, -1, -1, -2, -2, -3, -3, -1, -2, -4,  7, -1, -1, -4, -3, -2, -2, -1, -2, -4],
+[ 1, -1,  1,  0, -1,  0,  0,  0, -1, -2, -2,  0, -1, -2, -1,  4,  1, -3, -2, -2,  0,  0,  0, -4],
+[ 0, -1,  0, -1, -1, -1, -1, -2, -2, -1, -1, -1, -1, -2, -1,  1,  5, -2, -2,  0, -1, -1,  0, -4],
+[-3, -3, -4, -4, -2, -2, -3, -2, -2, -3, -2, -3, -1,  1, -4, -3, -2, 11,  2, -3, -4, -3, -2, -4],
+[-2, -2, -2, -3, -2, -1, -2, -3,  2, -1, -1, -2, -1,  3, -3, -2, -2,  2,  7, -1, -3, -2, -1, -4],
+[ 0, -3, -3, -3, -1, -2, -2, -3, -3,  3,  1, -2,  1, -1, -2, -2,  0, -3, -1,  4, -3, -2, -1, -4],
+[-2, -1,  3,  4, -3,  0,  1, -1,  0, -3, -4,  0, -3, -3, -2,  0, -1, -4, -3, -3,  4,  1, -1, -4],
+[-1,  0,  0,  1, -3,  3,  4, -2,  0, -3, -3,  1, -1, -3, -1,  0, -1, -3, -2, -2,  1,  4, -1, -4],
+[ 0, -1, -1, -1, -2, -1, -1, -1, -1, -1, -1, -1, -1, -1, -2,  0,  0, -2, -1, -1, -1, -1, -1, -4],
+[-4, -4, -4, -4, -4, -4, -4, -4, -4, -4, -4, -4, -4, -4, -4, -4, -4, -4, -4, -4, -4, -4, -4,  1],
+]
+edit_distance1=0
+edit_distance2=0
+edit_distance3=0
+#count the aminos that are different between each two sequences
+for a in range(len(human)):
+    if human[a]!=mouse[a]:
+        edit_distance1+=1
+for b in range(len(mouse)):
+    if mouse[b]!=random[b]:
+        edit_distance2+=1
+for c in range(len(random)):
+    if random[c]!=human[c]:
+        edit_distance3+=1
+
+#create a functon using the BLOSUM62 matrix
+def score(x,y):
+    scores=0
+    for i in range(0,len(x)):
+        a=amino.index(x[i])
+        b=amino.index(y[i])
+        scores += blosum[a][b]
+    return scores
+
+h_m=1-edit_distance1/len(human)
+h_r=1-edit_distance2/len(mouse)
+m_r=1-edit_distance3/len(random)
+
+print("The Hamming of human-mouse:",edit_distance1)
+print("The score of human-mouse:",score(human,mouse))
+print("The percentage of identical amino acids for human-mouse:",h_m*100,'%')
+print("The Hamming of human-random:",edit_distance2)
+print("The score of human-random:",score(human,random))
+print("The percentage of identical amino acids for human-random:",h_r*100,'%')
+print("The Hamming of mouse_random:",edit_distance3)
+print("The score of mouse-random:",score(mouse,random))
+print("The percentage of identical amino acids for mouse-random:",m_r*100,'%')
